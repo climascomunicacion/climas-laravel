41e75be4b12f51cd30444c0d26038bca905e97b2
climascomunicacion <jon@climascomunicacion.com>
2021-03-09 19:44:55 +0100

    enlaces footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,14 +3,14 @@
         <div class="container">
             <ul>
                 <li><p class="text-white">© CLI +</p></li>
-                <li><a href="">Información legal</a></li>
-                <li><a href="">Cookies</a></li>
+                <li><a href="{{ localized_route('legal') }}">Información legal</a></li>
+                <li><a href="{{ localized_route('cookies') }}">Cookies</a></li>
             </ul>
             <ul>
-                <li><a href="">Inicio</a></li>
-                <li><a href="">CLI +</a></li>
-                <li><a href="">Servicios</a></li>
-                <li><a href="">Contacto</a></li>
+                <li><a href="{{ localized_route('/') }}">Inicio</a></li>
+                <li><a href="{{ localized_route('climas') }}">CLI +</a></li>
+                <li><a href="{{ localized_route('services') }}">Servicios</a></li>
+                <li><a href="{{ localized_route('contact') }}">Contacto</a></li>
             </ul>
         </div>
         <div class="container">
