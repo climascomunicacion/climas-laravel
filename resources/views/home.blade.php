9ed1a5707ded7360e714008a5276840bae2437d6
climascomunicacion <jon@climascomunicacion.com>
2021-03-09 19:41:17 +0100

    cadenas traducibles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,7 +92,7 @@
     <div class="container">
         <div class=" bg-grey">
             <div class="txt">
-                <h2>¿Tomamos un café?</h2>
+                <h2>@lang('¿Tomamos un café?')</h2>
                 <a href="tel:666 888 999" class="tel">747 425 053</a>
                 <a class="link" href="mailto:hola@climascomunicacion.com">hola@climascomunicacion.com</a>
             </div>
@@ -108,7 +108,7 @@
 </section>
 <div id="box-cookies" class="bg-yellow">
     <div class="container">
-        <h4>Política de Cookies y Privacidad</h4>
+        <h4>@lang('Política de Cookies y Privacidad')</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit fames, porta justo arcu tempus porttitor at nunc tristique hac, purus semper ultricies tempor auctor sed luctus. Nostra et turpis dignissim class metus <a href="#">Política de Cookies</a></p>
         <a class="button" href="#">acepto
             <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="long-arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>
