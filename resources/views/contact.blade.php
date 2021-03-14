ae67aade9cf6b6591e7dbdadf8ebe2aeea86748a
iulene <iulene@climascomunicacion.com>
2021-03-14 10:50:19 +0100

    botón whatsapp y header

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,6 +6,16 @@
 
 @section('content')
      
+<!-- intro -->
+<section class="intro-content-pages bg-blue">
+    <div class="container ">
+        <h3 class="text-pink">contacto</h3>
+        <h2 class="text-white"><span class="uppercase">Cli</span> @lang('Escríbenos si quieres saber') <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="25pt" height="25pt" viewBox="0 0 177.000000 173.000000" preserveAspectRatio="xMidYMid meet"> <g transform="translate(0.000000,173.000000) scale(0.100000,-0.100000)" fill="#ce6a6b" stroke="none"><path d="M727 1705 c-20 -14 -47 -45 -59 -68 -22 -40 -23 -56 -26 -274 l-3 -233 -242 0 c-300 -1 -331 -9 -378 -102 -16 -31 -19 -59 -19 -163 0 -104 3 -132 19 -163 47 -93 78 -101 379 -102 l242 0 0 -300 c0 -216 3 -300 11 -300 6 0 42 11 79 24 100 36 209 56 310 57 l90 1 0 259 0 259 243 0 c300 1 331 9 378 102 16 31 19 59 19 163 0 104 -3 132 -19 163 -47 93 -78 101 -378 102 l-242 0 -3 233 c-3 218 -4 234 -26 274 -12 23 -39 54 -59 68 -35 23 -46 25 -158 25 -112 0 -123 -2 -158 -25z"/></g></svg>
+            </h2>
+            <img class="bg-mas" src="img/bg-mas.png">
+    </div>
+</section>   
+<!-- CONTENT -->
 <section class="services-container">
     <article class="que-necesitas">
         <div class="container">
@@ -21,7 +31,9 @@
                 <p class="uppercase">@lang('puedes contactarnos en:')</p>
                 <a href="tel:666 888 999" class="tel">747 425 053</a>
                 <a class="link" href="mailto:hola@climascomunicacion.com">hola@climascomunicacion.com</a>
-                <a class="link-whatsapp" href="https://wa.me/34747425053">whatsapp  <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="long-arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg></a>
+                <a class="link-whatsapp" href="https://wa.me/34747425053">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/></svg>
+                    whatsapp  </a>
             </div>
             <div class="form">
                 <p class="uppercase">@lang('Si lo prefieres rellena este formulario:')</p>
@@ -53,7 +65,7 @@
                         <label for="checkbox">@lang('He leído y acepto la') <a href="{{ localized_route('legal') }}" title="@lang('Leer infomración legal')">@lang('información legal')</a>
                         </label>
                         @if ($errors->has('checkbox'))
-                        <p class="error">@lang('Es necesario aceptar lainformación legal')</p>
+                        <p class="error">@lang('Es necesario aceptar la información legal')</p>
                         @endif
                     </div>
                     <div class="boton">
