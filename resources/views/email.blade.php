d111fcfc19181160d5798486a8be544bcb452ed9
iulene <iulene@climascomunicacion.com>
2021-03-18 18:06:14 +0100

    botón formulario y correciones

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -4,10 +4,10 @@
     </style>
 </head>
 <h2 class=·>Cli+ Comunicación</h2>
-<p>Email recivido de: {{ $nombre }}</p> 
+<p>Email recibido de: {{ $nombre }}</p> 
 
 <p style="color:#">Información del contacto:</p> 
 
     <p>Nombre: {{ $nombre }}</p> 
     <p>Email: {{ $email }}</p> 
-    <p>Mensage: {{ $comentario }}</p> 
+    <p>Mensaje: {{ $comentario }}</p> 
